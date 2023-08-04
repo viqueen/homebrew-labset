64f799cedadaa327ed496bf1c7deb783ed67c79f
viqueen <hasnae.rehioui@safetyculture.io>
2023-08-04 14:09:16 +1000

    upgrade to version 1.0.2 of git-workspaces

--- git-workspaces.rb
+++ git-workspaces.rb
@@ -3,8 +3,8 @@ require "language/node"
 class GitWorkspaces < Formula
   desc "Collection of useful git extensions that enhance one's local dev environment"
   homepage "https://github.com/viqueen/git-workspaces"
-  url "https://registry.npmjs.org/@labset/git-workspaces/-/git-workspaces-1.0.1.tgz"
-  sha256 "83c5178671aaa1104abc556b2647dfe974b9fd723899da4150167750a82f3e3e"
+  url "https://registry.npmjs.org/@labset/git-workspaces/-/git-workspaces-1.0.2.tgz"
+  sha256 "a3d02ae072cdc38857056dd4bdf434003341820218297153e84b8e2275cbc3ce"
   license "Apache-2.0"
 
   depends_on "node"
