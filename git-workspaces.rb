7fd7b4307f9ba4ed13db552203fb1eed3c9512eb
viqueen <hasnae@viqueen.org>
2024-12-20 20:36:06 +1100

    update formulas

--- git-workspaces.rb
+++ git-workspaces.rb
@@ -4,7 +4,7 @@ class GitWorkspaces < Formula
 	desc "Collection of useful git extensions that enhance one's local dev environment"
 	homepage "https://github.com/viqueen/git-workspaces"
 	url "https://registry.npmjs.org/@labset/git-workspaces/-/git-workspaces-4.2.2.tgz"
-	sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+	sha256 "93247deb8f370221283757424cd292c15e133579cd05424dd1f0dcc5a8c7562f"
 	license "Apache-2.0"
 
 	depends_on "node"
--- protoc-gen-graphql.rb
+++ protoc-gen-graphql.rb
@@ -4,7 +4,7 @@ class ProtocGenGraphql < Formula
 	desc "A protoc plugin to generate GraphQL schema from protobuf files"
 	homepage "https://github.com/viqueen/protoc-gen-graphql"
 	url "https://registry.npmjs.org/@labset/protoc-gen-graphql/-/protoc-gen-graphql-1.0.0.tgz"
-	sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+	sha256 "37e23a6ec2cc8f37fa2eabdbf5d776b4377762f032502393a5daf1a0e531fb97"
 	license "Apache-2.0"
 
 	depends_on "node"
